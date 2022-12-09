e6dc3f145c96f70b759bf2c101fec870bd7800fa
Galina Matvienko <galyagmgm@gmail.com>
2022-12-09 15:21:02 +0100

    lml

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,48 +7,53 @@ import { Contacts } from "./Contacts/Contacts";
 
 export function App() {
   const [contacts, setContacts] = useState([
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-   ]); 
+  ]);
   const [filter, setFilter] = useState('')
 
-const addContacts = ({ name, number }) => {
-  const showContacts = {name, number, id: nanoid(),}
-   console.log(showContacts)
-   const { contacts } = this.state;
-  if (contacts.find(contact => contact.name === name)) {
+
+
+  const addContacts = ({ name, number }) => {
+    const showContacts = { name, number, id: nanoid(), }
+    console.log(showContacts)
+    const { contacts } = this.state;
+    if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
       return;
-   }
-  else {
-    setContacts(contacts) ({
-       contacts: [showContacts,...contacts]
+    }
+    else {
+      setContacts(contacts)({
+        contacts: [showContacts, ...contacts]
         
-        })    }
+      })
+    }
+  }
 
  
-   const addFilter = this.handleAddFilter() 
-   return (
-             <Conteiner>
+    const addFilter = this.handleAddFilter()
+    return (
+      <Conteiner>
         <h1 style={{
           textAlign: 'center',
           color: 'red',
-        bacgroundColor:'green',
+          bacgroundColor: 'green',
         }}>Phonebook</h1>
-        <Form  addContacts={addContacts} />
+         <Form addContacts={addContacts} />
         <h2 style={{
           textAlign: 'center',
           color: 'red',
-        }}>Contacts</h2> 
+        }}>Contacts</h2>
         <Filter filter={filter} onChange={this.HandleChangeFilterInput} />
-          <Contacts  contacts={addFilter} onRemove={this.deliteContacts} />
+        <Contacts contacts={addFilter} onRemove={this.deliteContacts} />
       </Conteiner>
 
-    )}
+    )
+  
 
-
+}
 
 
 
